68dbb9f3d96bbe1832fb859d21ea358481931ea1
Mirjafor <bot398230@gmail.com>
2026-06-24 14:41:36 +0600

    Update author name in adboxonly.js

--- scripts/cmds/adboxonly.js
+++ scripts/cmds/adboxonly.js
@@ -3,7 +3,7 @@ module.exports = {
 		name: "onlyadminbox",
 		aliases: ["onlyadbox", "adboxonly", "adminboxonly"],
 		version: "1.3",
-		author: "NTKhang",
+		author: "𝙼𝙸𝚁𝙹𝙰𝙵𝙾𝚁",
 		countDown: 5,
 		role: 1,
 		description: {
