6533580e322c145bf955e0ae0bd6fc48b111726a
Mirjafor <bot398230@gmail.com>
2026-06-24 15:12:57 +0600

    Update author name in lyrics command

--- scripts/cmds/lyrics.js
+++ scripts/cmds/lyrics.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "lyrics",
     aliases: ["songlyrics"],
     version: "2.0",
-    author: "xalman",
+    author: "𝙼𝙸𝚁𝙹𝙰𝙵𝙾𝚁",
     countDown: 5,
     role: 0,
     shortDescription: "Get song lyrics",
