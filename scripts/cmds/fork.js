99ee231ab100ddd0710a5fd692b789ee52f0bf77
Mirjafor <bot398230@gmail.com>
2026-06-25 03:01:05 +0600

    Update bot owner and repository link in fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -14,12 +14,12 @@ module.exports = {
 
   langs: {
     en: {
-      current: "╭━━━〔 𝐆𝐎𝐀𝐓-𝐁𝐎𝐓-𝐕𝟐 〕━━━⦿\n┃\n┃ 👑 𝗢𝘄𝗻𝗲𝗿: 𝙼𝙸𝚁𝙹𝙰𝙵𝙾𝚁\n┃ 🔗 𝗥𝗲𝗽𝗼: %1\n┃ 💎 𝗦𝘁𝗮𝘁𝘂𝘀: always updating\n┃\n┃ ❝ 𝖳𝗁𝖾 𝖦𝗋𝖾𝖺𝗍𝖾𝗌𝗍 𝖮𝖿 𝖠𝗅𝗅 𝖳𝗂𝗆𝖾 𝖠𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝗈𝗇 ❞\n┃\n╰━━━━━━〔 📥 〕━━━━━━⦿"
+      current: "╭━━━〔 𝙼𝙸𝚁𝙹𝙰𝙵𝙾𝚁-𝐁𝐎𝐓-𝐕𝟐 〕━━━⦿\n┃\n┃ 👑 𝗢𝘄𝗻𝗲𝗿: 𝙼𝙸𝚁𝙹𝙰𝙵𝙾𝚁\n┃ 🔗 𝗥𝗲𝗽𝗼: %1\n┃ 💎 𝗦𝘁𝗮𝘁𝘂𝘀: always updating\n┃\n┃ ❝ 𝖳𝗁𝖾 𝖦𝗋𝖾𝖺𝗍𝖾𝗌𝗍 𝖮𝖿 𝖠𝗅𝗅 𝖳𝗂𝗆𝖾 𝖠𝗎𝗍𝗈𝗆𝖺𝗍𝗂𝗈𝗇 ❞\n┃\n╰━━━━━━〔 📥 〕━━━━━━⦿"
     }
   },
 
   onStart: async function ({ message, getLang }) {
-    const link = "https://github.com/goatbotnx/GOAT-BOT-V2";
+    const link = "https://github.com/tmirjafor6-prog/-10/commits?author=tmirjafor6-prog";
     return message.reply(getLang("current", link));
   },
 
